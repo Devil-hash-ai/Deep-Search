26ef2efdeeb56b695078dde4809108bf424b838f
Devil-hash-ai <2277135201@qq.com>
2025-05-25 04:02:01 +0800

    Add files via upload

---
+++ alpha_clip/__init__.py
@@ -0,0 +1,7 @@
+## License and Usage Restriction
+
+This repository includes code, models, or data derived from resources licensed under [CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/) and the CLIP license.  
+All components are **strictly for non-commercial research purposes**.  
+Please do not use any part of this project for commercial applications, deployments, or products.
+
+from .alpha_clip import *
